1a99b9184cb3496677599996f5c6bc365aa821e5
roflolilolmao <qjeanmon@gmail.com>
2018-07-07 05:08:36 +0200

    classes no longer stack

--- js/tools.js
+++ js/tools.js
@@ -1,3 +1,14 @@
 function addClass(elem, newClass){
   elem.setAttribute('class', elem.getAttribute('class') + " " + newClass);
 }
+
+function removeClass(element, class_)
+{
+  let classes = element.getAttribute('class').split(' ').filter(
+      function(c)
+      {
+        return c != class_;
+      });
+  
+  element.setAttribute('class', classes);
+}
