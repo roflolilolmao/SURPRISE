b80f6739439df528320ea2eb70cd6de16340e522
roflolilolmao <qjeanmon@gmail.com>
2018-07-07 03:53:34 +0200

    cest vraiment duguelasse

--- grid.js
+++ grid.js
@@ -49,6 +49,7 @@ function Map()
   
   this.generateMap = function()
   {
+    // generateEnclosingWalls
     for (let x = 0; x < this.columnCount; x++)
     {
       this.setCellValue(x, 0, 'wall');
@@ -59,12 +60,55 @@ function Map()
     {
       this.setCellValue(0, y, 'wall');
       this.setCellValue(this.columnCount - 1, y, 'wall');
-     }
+    }
+
+    // generateRandomInnerRooms    
+    let randomPointInRange = function(small_x, big_x, small_y, big_y)
+    {
+      let randomValueInRange = function(small, big)
+      {
+        let range = big - small;
+        return Math.round(Math.random() * range) + small;
+      }
+      
+      return {
+          x: randomValueInRange(small_x, big_x),
+          y: randomValueInRange(small_y, big_y)
+        }
+    }
+    
+    let roomCorner = randomPointInRange(2, this.columnCount - 3, 2, this.rowCount - 3);
+    
+    this.setCellValue(roomCorner.x, roomCorner.y, 'wall');
+    
+    let chooseDirection = function()
+    {
+      let lol = Math.round(Math.random())
+      return lol == 0 ? -1: 1;
+    }
+    
+    let direction_x = chooseDirection();
+    let direction_y = chooseDirection();
+    
+    for (let x = roomCorner.x; x > 0 && x < this.columnCount - 1; x += direction_x)
+    {
+      this.setCellValue(x, roomCorner.y, 'wall');
+    }
+    
+    for (let y = roomCorner.y; y > 0 && y < this.rowCount - 1; y += direction_y)
+    {
+      this.setCellValue(roomCorner.x, y, 'wall');
+    }
   };
 
   this.setCellValue = function(x, y, value)
   {
     this.grid[x][y].innerHTML = this.asciiMap[value];
     addClass(this.grid[x][y], value);
+  };
+  
+  this.getCellValue = function(x, y)
+  {
+    return this.grid[x][y].getAttribute('class').split(' ').slice(-1)[0];
   };
 }
