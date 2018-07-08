c23a0b8d3690c8db426eedf03e38d8e87dc67fbb
unknown <mtorsell@student.42.fr>
2018-07-08 04:37:08 +0200

    added rubies

--- js/Ruby.js
+++ js/Ruby.js
@@ -1,11 +1,11 @@
-function PowerUp(){
+function Ruby (){
   this.position = {x : 0, y : 0};
 
   this.spawner = function (){
     this.position.x = getRandomInt(0, gMap.columnCount);
     this.position.y = getRandomInt(0, gMap.rowCount);
     if (gMap.getCellValue(this.position) === 'empty') {
-      gMap.setCellValue(this.position, 'powerup');
+      gMap.setCellValue(this.position, 'ruby');
     }
     else
       this.spawner();
