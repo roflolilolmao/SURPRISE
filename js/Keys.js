da0bf96a3456be8dd0fe3ca024d7c26edb0ca033
Mattia Torsello <mtorsell@student.42.fr>
2019-02-02 23:49:48 +0100

    added a shitselect for changing keyboard layout

--- js/Keys.js
+++ js/Keys.js
@@ -1,3 +1,10 @@
+var gKeyboardLayout = "qwerty";
+
+function updateKeyboardLayout(e)
+{
+    gKeyboardLayout = e.target.value;
+}
+
 document.onkeydown = function (e){
 
   if (!gMusicActive)
@@ -7,20 +14,26 @@ document.onkeydown = function (e){
     
     gMusicActive = true;
   }
-  
-  if (e.key === 'w'){
-    gPlayer.move(0, -1);
+  if (gKeyboardLayout === "qwerty") {
+      if (e.key === 'w') {
+          gPlayer.move(0, -1);
+      }
+      else if (e.key === 's') {
+          gPlayer.move(0, 1);
+      }
+      else if (e.key === 'a') {
+          gPlayer.move(-1, 0);
+      }
+      else if (e.key === 'd') {
+          gPlayer.move(1, 0);
+      }
+      else if (e.key === 'q' || e.key === 'p') {
+          gPlayer.attack();
+      }
   }
-  else if (e.key === 's'){
-    gPlayer.move(0, 1);
-  }
-  else if (e.key === 'a'){
-    gPlayer.move(-1, 0);
-  }
-  else if (e.key === 'd'){
-    gPlayer.move(1, 0);
-  }
-  else if (e.key === 'q' || e.key === 'p'){
-    gPlayer.attack();
-  }
+};
+
+document.onkeyup = function (e)
+{
+
 };
