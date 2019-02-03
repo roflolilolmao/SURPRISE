d915e2f1e797b6cfa9c97a752ba67ba8c22d2bc6
Mattia Torsello <mtorsell@student.42.fr>
2019-02-03 01:45:43 +0100

    on a fait des choses madudes

--- js/Keys.js
+++ js/Keys.js
@@ -1,40 +1,52 @@
-var gKeyboardLayout = "qwerty";
+let currentKeyboardLayout = "qwerty";
+let keyState = {};
+const REPEAT_FREQUENCY = 6;
+
+const KEYBOARD_LAYOUTS =
+    {
+        'qwerty':
+            {
+                'w': 'up',
+                'a': 'left',
+                's': 'down',
+                'd': 'right',
+                'p': 'hit',
+                'q': 'hit'
+            },
+        'dvorak':
+            {
+                ',': 'up',
+                'a': 'left',
+                'o': 'down',
+                'e': 'right',
+                'l': 'hit',
+                '\'': 'hit'
+            }
+    };
+
+const KEYBOARD_INPUTS =
+    {
+        'up': function () { gPlayer.move(0, -1); },
+        'left': function () { gPlayer.move(-1, 0); },
+        'down': function () { gPlayer.move(0, 1); },
+        'right': function () { gPlayer.move(1, 0); },
+        'hit': function () { gPlayer.attack(); }
+    };
 
 function updateKeyboardLayout(e)
 {
-    gKeyboardLayout = e.target.value;
+  currentKeyboardLayout = e.target.value;
 }
 
-let keyboardLayouts =
-    {
-      'qwerty':
-          {
-            'w': 'up',
-            'a': 'left',
-            's': 'down',
-            'd': 'right',
-            'p': 'hit',
-            'q': 'hit'
-          },
-      'dvorak':
-          {
-            ',': 'up',
-            'a': 'left',
-            'o': 'down',
-            'e': 'right',
-            'l': 'hit',
-            '\'': 'hit'
-          }
-    };
-
-let keyboardInputs =
-    {
-      'up': function () { gPlayer.move(0, -1); },
-      'left': function () { gPlayer.move(-1, 0); },
-      'down': function () { gPlayer.move(0, 1); },
-      'right': function () { gPlayer.move(1, 0); },
-      'hit': function () { gPlayer.attack(); }
-    }
+function removeAndStopKeyboardActions()
+{
+  document.onkeydown = function (){};
+  for (let key in keyState)
+  {
+    if (keyState.hasOwnProperty(key))
+      clearInterval(keyState[key]);
+  }
+}
 
 document.onkeydown = function (e)
 {
@@ -42,17 +54,24 @@ document.onkeydown = function (e)
   {
     gCoolBassLine.loop = true;
     gCoolBassLine.play();
-    
+
     gMusicActive = true;
   }
 
-  let keyboardInput = keyboardLayouts[gKeyboardLayout][e.key];
-  if (!(keyboardInput in keyboardInputs))
+  let keyboardInput = KEYBOARD_LAYOUTS[currentKeyboardLayout][e.key];
+
+  if (!(keyboardInput in KEYBOARD_INPUTS))
     return;
 
-  keyboardInputs[keyboardInput]();
+  if (!keyState[e.key])
+  {
+    KEYBOARD_INPUTS[keyboardInput]();
+    keyState[e.key] = setInterval(KEYBOARD_INPUTS[keyboardInput], 1000 / REPEAT_FREQUENCY);
+  }
 };
 
 document.onkeyup = function (e)
 {
+  clearInterval(keyState[e.key]);
+  keyState[e.key] = null;
 };
